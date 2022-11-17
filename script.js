dc642727b2e109f7407bf3a5ba1c0fc482482c85
Marcus Bynoe <marcusbynoe57@gmail.com>
2022-11-17 14:02:15 -0700

    added js stuff

---
+++ script.js
@@ -0,0 +1,14 @@
+function hello() {
+     let usersName = prompt("What is your name?");
+        console.log("The user's name is:" + usersName);
+        document.write("Welcome to burger joint, " + usersName);
+}
+
+
+function likeBurgers() {
+    let response = prompt("Do you like burgers?")
+     if (response == "yes") {
+    alert("Welcome to the promise land of burgers!");
+    document.write("Welcome to the promise land of burgers!");
+    }
+} 
