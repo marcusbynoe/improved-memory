2832c5abd6fb2a6175d9584ba3c2e5ed70a53e5a
Marcus Bynoe <marcusbynoe57@gmail.com>
2022-11-17 14:30:15 -0700

    added response if no name is given

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
-function hello() {
-     let usersName = prompt("What is your name?");
-        console.log("The user's name is:" + usersName);
-        document.write("Welcome to burger joint, " + usersName);
+function hello() {  
+    let usersName = prompt("What is your name?");
+   if (usersName == "") {
+   alert("You didn't type a name...");
+   usersName = prompt("Please tell me your name.");
+   console.log("The user's name is:" + usersName);
+   document.write("Welcome to burger joint, " + usersName);} 
 }
 
 
+
 function likeBurgers() {
-    let response = prompt("Do you like burgers?")
+    let response = prompt("Do you like burgers?");
      if (response == "yes") {
     alert("Welcome to the promise land of burgers!");
     document.write("Welcome to the promise land of burgers!");
+    } else if (response == "no") {
+        alert("Hopefully I can change your mind, welcome to burger joint!");
+        document.write("Hopefully I can change your mind, welcome to burger joint!");
+    } else {
+        alert("Not sure what you said...");
     }
 } 
